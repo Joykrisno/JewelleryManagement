7efb373c525af16a499eb35de54cb99ab229e8f3
Joykrisno <97521673+Joykrisno@users.noreply.github.com>
2026-03-14 20:45:25 +0600

    update

--- obj/Debug/net8.0/JewelleryManagement.AssemblyInfo.cs
+++ obj/Debug/net8.0/JewelleryManagement.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JewelleryManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+258dca246e34ba6363b1d2a1a44ab8a7f061a43e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83c01eab4dc8da3bec4ccd0ac15c351d9ec64fd5")]
 [assembly: System.Reflection.AssemblyProductAttribute("JewelleryManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JewelleryManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
